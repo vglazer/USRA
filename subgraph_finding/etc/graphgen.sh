780e43aaceeb380fa5ef1c9831f21c40cadc9508
Victor Glazer <victor.glazer@gmail.com>
2020-08-23 22:49:48 -0400

    Factor out inputs into configutation files

--- subgraph_finding/etc/graphgen.sh
+++ subgraph_finding/etc/graphgen.sh
@@ -1,28 +1,45 @@
 #! /usr/bin/env bash
 
+scriptname=$(basename $0)
+
 # the top-level repo directory is two levels up from the etc subdirectory 
 repo_dir=$(dirname $(dirname $(realpath $0)))
 bin_dir=$repo_dir/bin
+etc_dir=$repo_dir/etc
 base_graphs_dir=$repo_dir/graphs
+
+# awk should be there, but you never know
+awk_location=`/usr/bin/which awk`
+if [ "$awk_location" = "" ]; then
+    echo "$scriptname could not find awk; bailing..." >&2
+    exit 1
+fi
 
 # generate sample unweighted random graphs using ggen
 if [ -f $bin_dir/ggen ]; then 
     graphs_dir_unweighted=$base_graphs_dir/unweighted
     mkdir -p $graphs_dir_unweighted
-    echo "2 100 0 600 2 0 0 0" | $bin_dir/ggen > $graphs_dir_unweighted/exponential_100.txt
-    echo "3 2500 0 200 52 0 0 0" | $bin_dir/ggen > $graphs_dir_unweighted/power_2500.txt
-    echo "3 2500 0 200 52 0 0 1" | $bin_dir/ggen > $graphs_dir_unweighted/power_2500_complement.txt
-    echo "4 790 0 150 1 0 0 0" | $bin_dir/ggen > $graphs_dir_unweighted/geometric_790.txt
+
+    while read line; do
+        inputs=`echo "$line" | awk '{ $NF=""; print $0 }'`
+        filename=`echo "$line" | awk '{ print $NF }'`
+        echo $inputs | $bin_dir/ggen > $graphs_dir_unweighted/$filename
+    done <$etc_dir/ggen_inputs.txt 
 else
-    echo "$bin_dir/ggen does not exist. try running make ggen"
+    echo "$bin_dir/ggen does not exist. try running make ggen" >&2
 fi
 
 # generate sample weighted random graphs using wggen
 if [ -f $bin_dir/wggen ]; then 
     graphs_dir_weighted=$base_graphs_dir/weighted
     mkdir -p $graphs_dir_weighted
-    $bin_dir/wggen 200 65 1 > $graphs_dir_weighted/exponential_200.txt
-    $bin_dir/wggen 1500 300 72 > $graphs_dir_weighted/exponential_1500.txt
+    while read line; do
+        inputs=`echo "$line" | awk '{ $NF=""; print $0 }'`
+        filename=`echo "$line" | awk '{ print $NF }'`
+        $bin_dir/wggen $inputs > $graphs_dir_weighted/$filename
+    done <$etc_dir/wggen_inputs.txt     
 else
-    echo "$bin_dir/wggen does not exist. try running make wggen"
+    echo "$bin_dir/wggen does not exist. try running make wggen" >&2
 fi
+
+exit 0
