737c3bd6005ca1856972caee7a2d26a3409810af
Victor Glazer <victor.glazer@gmail.com>
2020-08-25 20:59:57 -0400

    And now, let's quote every interpolated variable

--- subgraph_finding/etc/graphgen.sh
+++ subgraph_finding/etc/graphgen.sh
@@ -4,12 +4,12 @@ scriptname=$(basename "$0")
 
 # the top-level repo directory is two levels up from the etc subdirectory 
 repo_dir=$(dirname "$(dirname "$(realpath "$0")")")
-bin_dir="$repo_dir/bin"
-etc_dir="$repo_dir/etc"
-base_graphs_dir="$repo_dir/graphs"
+bin_dir="${repo_dir}/bin"
+etc_dir="${repo_dir}/etc"
+base_graphs_dir="${repo_dir}/graphs"
 
 if [[ $# -ne 0 ]]; then
-    echo "$scriptname takes no arguments"
+    echo "${scriptname} takes no arguments"
     exit 1
 fi
 
@@ -20,41 +20,41 @@ function generate_graphs {
     local kind="$4"
     local pipe_args="$5"
 
-    if [[ -f "$generator" ]]; then 
-        graphs_dir="$base_graphs_dir/$kind"
-        mkdir -p "$graphs_dir"
+    if [[ -f "${generator}" ]]; then 
+        graphs_dir="${base_graphs_dir}/${kind}"
+        mkdir -p "${graphs_dir}"
 
         while read -r line; do
             # the last token is the output file. everything else is a generator argument
-            args=$(echo "$line" | awk '{ $NF=""; print $0 }')
-            output_file=$(echo "$line" | awk '{ print $NF }')
+            args=$(echo "${line}" | awk '{ $NF=""; print $0 }')
+            output_file=$(echo "${line}" | awk '{ print $NF }')
 
-            if [[ "$pipe_args" = 'true' ]]; then
-                echo "$args" | $generator > "$graphs_dir/$output_file"
+            if [[ "${pipe_args}" = 'true' ]]; then
+                echo "${args}" | ${generator} > "${graphs_dir}/${output_file}"
             else 
-                $generator "$args" > "$graphs_dir/$output_file"
+                ${generator} "${args}" > "${graphs_dir}/${output_file}"
             fi
-        done <"$input_file"
+        done <"${input_file}"
 
         return 0
     else
         local funcname="${FUNCNAME[0]}"
 
         local binary
-        binary=$(basename "$generator")
+        binary=$(basename "${generator}")
 
-        echo "$funcname: $binary not found. try running make $binary" >&2
+        echo "${funcname}: ${binary} not found. try running make ${binary}" >&2
 
         return 1
     fi
 }
 
-generate_graphs "$bin_dir/ggen" "$etc_dir/ggen_inputs.txt" "$base_graphs_dir" 'unweighted' 'true'
+generate_graphs "${bin_dir}/ggen" "${etc_dir}/ggen_inputs.txt" "${base_graphs_dir}" 'unweighted' 'true'
 ggen_res="$?"
 
-generate_graphs "$bin_dir/wggen" "$etc_dir/wggen_inputs.txt" "$base_graphs_dir" 'weighted' 'false'
+generate_graphs "${bin_dir}/wggen" "${etc_dir}/wggen_inputs.txt" "${base_graphs_dir}" 'weighted' 'false'
 wggen_res="$?"
 
 # return 0 if all generator commands succeeded, failure count otherwise
 retval=$((ggen_res + wggen_res))
-exit "$retval"
+exit "${retval}"
