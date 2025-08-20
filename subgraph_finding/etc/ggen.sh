322fbf0763a7c7eabc6fe5887ded5da8a1ddecf3
Victor Glazer <victor.glazer@gmail.com>
2025-08-20 16:11:45 -0400

    Better error handling

--- subgraph_finding/etc/ggen.sh
+++ subgraph_finding/etc/ggen.sh
@@ -8,37 +8,37 @@ BUILD_COMMAND="cd $REPO_DIR; make $GGEN_BINARY"
 
 function handle_error {
     local error_message
-    if [ $# -ge 1 ]; then
+    if [[ $# -ge 1 ]]; then
         error_message=$1
     else
         error_message="Someting went wrong"
     fi
 
+    local have_fix=0
     local fix_message
-    if [ $# -ge 2 ]; then
+    if [[ $# -ge 2 ]]; then
+        have_fix=1
         fix_message=$2
-    else
-        fix_message="$BUILD_COMMAND"
     fi
 
-    echo "Error: $error_message"
-    echo "Fix: $fix_message"
+    local func_name="${FUNCNAME[0]}() ~"
+    echo "$func_name Error: $error_message"
+    if (( have_fix )); then
+        echo "$func_name Fix: $fix_message"
+    fi
     
     exit 1
 }
 
 # main
 
-if [ ! -d "$BIN_DIR" ]; then
-    handle_error "$BIN_DIR not found"
+if [[ ! -d "$BIN_DIR" ]]; then
+    handle_error "$BIN_DIR not found" "$BUILD_COMMAND"
 fi
-
-# To resolve paths such as etc/../bin/
-BIN_DIR=$(realpath "$BIN_DIR")
 
 GGEN_PATH="$BIN_DIR/$GGEN_BINARY"
 if [[ ! -f "$GGEN_PATH" ]]; then
-    handle_error "$GGEN_PATH not found"
+    handle_error "$GGEN_PATH not found" "$BUILD_COMMAND"
 fi
 
 echo "$GGEN_BINARY" found: "$GGEN_PATH"
