c55e52a93baa71ce7f6a671279fe674fc85bc245
Victor Glazer <victor.glazer@gmail.com>
2025-09-03 17:09:06 -0400

    Layout better suited to large graphs

--- subgraph_finding/etc/edges2dot.sh
+++ subgraph_finding/etc/edges2dot.sh
@@ -18,8 +18,9 @@ fi
 edges_path=$1
 edges_dir=$(dirname "$edges_path")
 edges_file=$(basename "$edges_path")
+layout="sfdp"
 if [[ $edges_file =~ ^edges_(.+).txt$ ]]; then
-    graphviz_file="graphviz_${BASH_REMATCH[1]}.dot"
+    graphviz_file="${layout}_${BASH_REMATCH[1]}.dot"
     graphviz_path="$edges_dir/$graphviz_file"
 else
     echo "$script_name: expected edges_file to match edges_*.txt, got $edges_file" >&2
@@ -27,12 +28,20 @@ else
 fi
 
 awk_script='
-  BEGIN { print "graph G {" }
+  BEGIN { print "graph G {" 
+    print "  layout=" layout ";"
+    print "  sep=\"+150,150\";"
+    print "  overlap=false;"
+    print "  splines=true;"
+    #print "  size=\"30,30\";"
+    print "  node [shape=point, width=0.05, height=0.05];"
+    print
+  }
 
   # undirected graph
   { print "  " $1 " -- " $2 ";" }
 
   END { print "}" }
 '
-cat "$edges_path" | awk -F',' -v graphviz_path="$graphviz_path" "$awk_script" > "$graphviz_path"
+cat "$edges_path" | awk -F',' -v layout="$layout" "$awk_script" > "$graphviz_path"
 echo "$graphviz_path"
