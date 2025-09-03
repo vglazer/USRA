260251bb0a7015908043fe124dd2b7b7c3a3263f
Victor Glazer <victor.glazer@gmail.com>
2025-09-03 15:23:57 -0400

    Format changes

--- subgraph_finding/etc/ggen_check.sh
+++ subgraph_finding/etc/ggen_check.sh
@@ -9,28 +9,35 @@ if (( $# < 1 )); then
 Usage: $script_name graph_file
 
 Arguments:  
-  graph_file  File containing graph to check. Must be named graph_foo.txt
+  graph_file  Path to file containing graph to check. Filename must match graph_*.txt
 
 EOF
   exit 1
 fi
 
-graph_file=$1
+graph_path=$1
+graph_dir=$(dirname "$graph_path")
+graph_file=$(basename "$graph_path")
 if [[ $graph_file =~ ^graph_(.+).txt$ ]]; then
     edges_file="edges_${BASH_REMATCH[1]}.txt"
+    edges_path="$graph_dir/$edges_file"
 else
-    echo "$script_name: expected graph_file to match graph_foo.txt, got $graph_file" >&2
+    echo "$script_name: expected graph_file to match graph_*.txt, got $graph_file" >&2
     exit 1
 fi
 
+# needed because we are appending edges
+rm -f "$edges_path"
+
 awk_script='{
-nedges += NF;
-vertex = NR-1;
-print vertex ":" $0";\n" vertex " -> " NF; 
+  nedges += NF;
+  vertex = NR-1;
+  print vertex ":" $0";\n" vertex " -> " NF; 
 
-for (i = 1; i <= NF; i++) 
-  print "{" vertex "," $i "}" >> edges_file 
-}
+  for (i = 1; i <= NF; i++) 
+    print vertex "," $i >> edges_path 
+  }
 
-END {print nedges}'
-cat "$graph_file" | sed 's/-1//g' | awk -v edges_file="$edges_file" "$awk_script"
+  END {print nedges}'
+cat "$graph_path" | sed 's/-1//g' | awk -v edges_path="$edges_path" "$awk_script"
+echo "$edges_path"
