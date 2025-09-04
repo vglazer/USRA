b264cd5000b980ba1a0b8fbf7a54960da4e20415
Victor Glazer <victor.glazer@gmail.com>
2025-09-04 00:21:07 -0400

    no styling

--- subgraph_finding/etc/edges2dot.sh
+++ subgraph_finding/etc/edges2dot.sh
@@ -4,16 +4,12 @@
 set -euo pipefail
 
 script_name=$(basename "$0")
-if (( $# != 5 )); then
+if (( $# != 1 )); then
   cat >&2 <<EOF
-Usage: $script_name edges_file sep width shape layout
+Usage: $script_name edges_file
 
 Arguments:  
   edges_file  Path to file containing graph edges. Filename must match edges_*.txt
-  sep         Graphviz sep parameter (150, 5)
-  width       Graphviz node width (and height) parameter (0.05, 0.5)
-  shape       Graphviz node shape parameter (point, circle)
-  layout      Graphviz layout engine (sfdp, neato)
 
 EOF
   exit 1
@@ -22,28 +18,17 @@ fi
 edges_path=$1
 edges_dir=$(dirname "$edges_path")
 edges_file=$(basename "$edges_path")
-layout=${5:-"$default_layout"}
 if [[ $edges_file =~ ^edges_(.+).txt$ ]]; then
-    graphviz_file="${layout}_${BASH_REMATCH[1]}.dot"
+    graphviz_file="no_styling_${BASH_REMATCH[1]}.dot"
     graphviz_path="$edges_dir/$graphviz_file"
 else
     echo "$script_name: expected edges_file to match edges_*.txt, got $edges_file" >&2
     exit 1
 fi
 
-sep=$2
-width=$3
-shape=$4
 awk_script='
   BEGIN {
-    height=width;
     print "graph G {"; 
-    print "  layout=" layout ";"
-    print "  sep=\"+" sep "," sep "\";"
-    print "  overlap=false;"
-    print "  splines=true;"
-    print "  node [shape=" shape ", width=" width ", height=" height "];"
-    print
 
     edge=0;
   }
@@ -61,5 +46,5 @@ awk_script='
     print edge > "/dev/stderr" 
   }
 '
-cat "$edges_path" | awk -F',' -v shape="$shape" -v layout="$layout" -v sep="$sep" -v width="$width" "$awk_script" > "$graphviz_path"
+cat "$edges_path" | awk -F',' "$awk_script" > "$graphviz_path"
 echo "$graphviz_path"
