71b9faebdf1a88c5343557e257b7e1fd6438bfca
Victor Glazer <victor.glazer@gmail.com>
2025-09-09 19:45:32 -0400

    Reordered arguments

--- subgraph_finding/etc/edges2dot.sh
+++ subgraph_finding/etc/edges2dot.sh
@@ -4,22 +4,23 @@
 set -euo pipefail
 
 script_name=$(basename "$0")
-default_sep=150
-default_width=0.05
-default_shape="point"
+
 default_layout="sfdp"
+default_sep=150
 default_splines="false"
+default_shape="point"
+default_width=0.05
 if (( $# < 1 || $# > 6 )); then
   cat >&2 <<EOF
-Usage: $script_name edges_file sep width shape layout splines
+Usage: $script_name edges_file layout sep splines shape width
 
 Arguments:  
   edges_file  Path to file containing graph edges. Filename must match edges_*.csv
-  sep         Graphviz sep parameter (150, 5), default: $default_sep
-  width       Graphviz node width (and height) parameter (0.05, 0.5), default: $default_width
-  shape       Graphviz node shape parameter (point, circle), default: $default_shape
   layout      Graphviz layout engine (sfdp, neato), default: $default_layout
+  sep         Graphviz sep parameter (150, 5), default: $default_sep
   splines     Graphviz splines parameter (false, true, curved), default: $default_splines
+  shape       Graphviz node shape parameter (point, circle), default: $default_shape
+  width       Graphviz node width (and height) parameter (0.05, 0.5), default: $default_width
 
 EOF
   exit 1
@@ -28,7 +29,7 @@ fi
 edges_path=$1
 edges_dir=$(dirname "$edges_path")
 edges_file=$(basename "$edges_path")
-layout=${5:-"$default_layout"}
+layout=${2:-"$default_layout"}
 if [[ $edges_file =~ ^edges_(.+).csv$ ]]; then
     graphviz_file="${layout}_${BASH_REMATCH[1]}.dot"
     graphviz_path="$edges_dir/$graphviz_file"
@@ -37,10 +38,10 @@ else
     exit 1
 fi
 
-sep=${2:-"$default_sep"}
-width=${3:-"$default_width"}
-shape=${4:-"$default_shape"}
-splines=${6:-"$default_splines"}
+sep=${3:-"$default_sep"}
+splines=${4:-"$default_splines"}
+shape=${5:-"$default_shape"}
+width=${6:-"$default_width"}
 awk_script='
   BEGIN {
     height=width
@@ -66,5 +67,5 @@ awk_script='
     print nedges > "/dev/stderr"
   }'
 
-cat "$edges_path" | awk -F',' -v shape="$shape" -v layout="$layout" -v sep="$sep" -v width="$width" -v splines="$splines" "$awk_script" > "$graphviz_path"
+cat "$edges_path" | awk -F',' -v layout="$layout" -v sep="$sep" -v splines="$splines" -v shape="$shape" -v width="$width" "$awk_script" > "$graphviz_path"
 echo "$graphviz_path"
