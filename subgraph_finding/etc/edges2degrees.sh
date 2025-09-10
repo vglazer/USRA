668a92b273771f35f7becceb7f6c8ca5f3735ca7
Victor Glazer <victor.glazer@gmail.com>
2025-09-10 15:48:57 -0400

    Handle degree 0 vertices properly

--- subgraph_finding/etc/edges2degrees.sh
+++ subgraph_finding/etc/edges2degrees.sh
@@ -5,32 +5,42 @@ set -euo pipefail
 
 script_name=$(basename "$0")
 
-if (( $# != 1 )); then
+if (( $# != 2 )); then
   cat >&2 <<EOF
-Usage: $script_name edges_file sep width shape layout
+Usage: $script_name v edges_file
 
-Arguments:  
+Arguments:
+  v           Number of vertices
   edges_file  Path to file containing graph edges. Filename must match edges_*.csv
 
 EOF
   exit 1
 fi
 
-edges_path=$1
+v=$1
+edges_path=$2
 edges_dir=$(dirname "$edges_path")
 edges_file=$(basename "$edges_path")
 if [[ $edges_file =~ ^edges_(.+).csv$ ]]; then
-    degrees_file="degrees_${BASH_REMATCH[1]}.csv"
+    signature="${BASH_REMATCH[1]}"
+    degrees_file="degrees_$signature.csv"
     degrees_path="$edges_dir/$degrees_file"
 
-    degrees_count_file="degree_counts_${BASH_REMATCH[1]}.csv"
+    degrees_count_file="degree_counts_$signature.csv"
     degrees_count_path="$edges_dir/$degrees_count_file"
 else
     echo "$script_name: expected edges_file to match edges_*.csv, got $edges_file" >&2
     exit 1
 fi
 
-awk_script_degrees='{
+awk_script_degrees='
+BEGIN {
+  for (i = 0; i < v; i++) {
+    degrees[i] = 0
+  }
+}
+
+{
   degrees[$1]++
   degrees[$2]++
 
@@ -51,7 +61,7 @@ END {
   print degree_sum > "/dev/stderr"
 }'
 
-cat "$edges_path" | awk -F ',' "$awk_script_degrees" | sort -t',' -k1,1n > "$degrees_path"
+cat "$edges_path" | awk -F ',' -v v="$v" "$awk_script_degrees" | sort -t',' -k1,1n > "$degrees_path"
 
 # compute degree counts by inverting the degrees hash
 awk_script_counts='{
